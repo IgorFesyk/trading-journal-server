c079337ce9013f028b85a0820e114871fbaa2b39
Ihor Fesyk <igor.fesyk.g@gmail.com>
2026-07-08 23:23:04 +0300

    workaround for the db connection

--- src/infra/prisma.ts
+++ src/infra/prisma.ts
@@ -4,7 +4,10 @@ import { Pool } from 'pg';
 import { env } from '../env';
 import { PrismaClient } from '../generated/prisma/client';
 
-const pool = new Pool({ connectionString: env.POSTGRES_URL });
+const pool = new Pool({
+    connectionString: env.POSTGRES_URL,
+    ssl: env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
 const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({ adapter });
